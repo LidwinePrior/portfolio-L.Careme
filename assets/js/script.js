8daf50150159ef48f65730a16bb49a11b9e85de1
Lidwine Carême <lidwine.careme@gmail.com>
2024-01-24 11:18:39 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,25 @@ function toggleMobileMenu() {
 }
 
 
-function closeMenu() {
+function closeMobileMenu() {
     var mobileNav = document.querySelector('.mobile-nav');
     mobileNav.style.display = 'none';
 }
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Ajoutez la classe "show" après un certain délai (par exemple, 500 millisecondes)
+    setTimeout(function () {
+        document.querySelector('h1').classList.add('show');
+        document.querySelector('h2').classList.add('show');
+        document.querySelectorAll('h3').forEach(function (element) {
+            element.classList.add('show');
+        });   
+    }, 500);
+});
+
